754ba8505406373002876104779657495746715f
zjl <junlinzhou@yzbigdata.com>
2023-09-02 11:13:43 +0800

    update: intermidiate thought streaming strategy

--- api/sqlbot/callbacks/thought.py
+++ api/sqlbot/callbacks/thought.py
@@ -6,20 +6,21 @@ from fastapi import WebSocket
 from sqlbot.callbacks.base import WebsocketCallbackHandler
 from sqlbot.schemas import ChatMessage
 
-DEFAULT_THOUGHT_PREFIX_TOKENS = ["Th", "ought", ":"]
+DEFAULT_THOUGHT_SUFFIX_TOKENS = "Action"
 
 
 class StreamingIntermediateThoughtCallbackHandler(WebsocketCallbackHandler):
     """Streaming intermediate thought to websocket.
-    Based on `langchain.callbacks.streaming_stdout_final_only.FinalStreamingStdOutCallbackHandler`
+    Typically, during agent execution, the initial messages represent intermediate thoughts, which are followed by an "action."
+    This callback handler streams every token up to the "Action" to the websocket.
+    It's important to note that certain formatting instructions may also stream a 'Thought:' in the initial round.
     """
 
     def __init__(
         self,
         websocket: WebSocket,
         conversation_id: str,
-        thought_prefix_tokens: Optional[list[str]] = None,
-        strip_tokens: bool = True,
+        thought_suffix_token: Optional[str] = None,
     ) -> None:
         """Instantiate StreamingIntermediateThoughtCallbackHandler.
 
@@ -31,36 +32,32 @@ class StreamingIntermediateThoughtCallbackHandler(WebsocketCallbackHandler):
                 reached)
         """
         super().__init__(websocket, conversation_id)
-        if thought_prefix_tokens is None:
-            self.thought_prefix_tokens = DEFAULT_THOUGHT_PREFIX_TOKENS
+        if thought_suffix_token is None:
+            self.thought_suffix_token = DEFAULT_THOUGHT_SUFFIX_TOKENS
         else:
-            self.thought_prefix_tokens = thought_prefix_tokens
-        if strip_tokens:
-            self.thought_prefix_tokens = [
-                token.strip() for token in self.thought_prefix_tokens
-            ]
-        self.strip_tokens = strip_tokens
-
-    def check_if_thought_reached(self) -> bool:
-        return self.last_tokens == self.thought_prefix_tokens
-
-    def append_to_last_tokens(self, token: str) -> None:
-        if self.strip_tokens:
-            token = token.strip()
-            if token:
-                self.last_tokens.append(token)
-        else:
-            self.last_tokens.append(token)
-        if len(self.last_tokens) > len(self.thought_prefix_tokens):
-            self.last_tokens.pop(0)
+            self.thought_suffix_token = thought_suffix_token
 
     async def on_llm_start(
-        self, serialized: dict[str, Any], prompts: list[str], **kwargs: Any
+        self,
+        serialized: dict[str, Any],
+        prompts: list[str],
+        *,
+        run_id: UUID,
+        parent_run_id: Optional[UUID] = None,
+        tags: Optional[list[str]] = None,
+        metadata: Optional[dict[str, Any]] = None,
+        **kwargs: Any,
     ) -> None:
         """Run when LLM starts running."""
-        self.last_tokens = [""] * len(self.thought_prefix_tokens)
-        self.thought_reached = False
-        self.thought_ended = False
+        self.thinking = True
+        message = ChatMessage(
+            id=run_id,
+            conversation=self.conversation_id,
+            from_="ai",
+            content=None,
+            type="start",
+        )
+        await self.websocket.send_json(message.dict())
 
     async def on_llm_new_token(
         self,
@@ -72,35 +69,17 @@ class StreamingIntermediateThoughtCallbackHandler(WebsocketCallbackHandler):
         **kwargs: Any,
     ) -> None:
         """Run on new LLM token. Only available when streaming is enabled."""
-        if token == "Action":
-            self.thought_ended = True
+        if token == self.thought_suffix_token:
+            self.thinking = False
 
-        if self.thought_ended:
+        if not self.thinking:
             return
 
-        # Remember the last n tokens, where n = len(answer_prefix_tokens)
-        self.append_to_last_tokens(token)
-
-        # Check if the last n tokens match the answer_prefix_tokens list ...
-        if self.check_if_thought_reached():
-            self.thought_reached = True
-            message = ChatMessage(
-                id=run_id,
-                conversation=self.conversation_id,
-                from_="ai",
-                content=None,
-                type="start",
-            )
-            await self.websocket.send_json(message.dict())
-            return
-
-        # ... if yes, then print tokens from now on
-        if self.thought_reached:
-            message = ChatMessage(
-                id=run_id,
-                conversation=self.conversation_id,
-                from_="ai",
-                content=token,
-                type="stream",
-            )
-            await self.websocket.send_json(message.dict())
+        message = ChatMessage(
+            id=run_id,
+            conversation=self.conversation_id,
+            from_="ai",
+            content=token,
+            type="stream",
+        )
+        await self.websocket.send_json(message.dict())
