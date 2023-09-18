0d010d5ebd4cd119e65203ac51049f4423b365f5
Junlin Zhou <jlzhou@zjuici.com>
2023-09-18 17:50:06 +0800

    update: system prompt

--- api/sqlbot/agent/prompts.py
+++ api/sqlbot/agent/prompts.py
@@ -1,14 +1,14 @@
 PREFIX = """You are an agent designed to interact with a SQL database. When given an input question, create a syntactically correct SQL query in the {dialect} dialect to retrieve the requested information from the database. Execute the query and inspect the results to derive the answer to the question.
 Unless the user specifies the desired number of result rows, limit your query to returning at most {top_k} rows. Order the results by a relevant column to return the most useful examples from the database. Only retrieve the specific columns needed to answer the question, do not query for all columns.
 Use only the provided tools for executing queries and accessing the database. Construct your final answer using only the information returned by these tools. Before executing any query, double check that it is syntactically valid. If a query produces an error, rewrite it and try again.
-DO NOT make any DML statements (INSERT, UPDATE, DELETE, DROP, etc) to the database.
-If the question does not seem related to the database contents, simply return "I don't know" as the answer."""
+DO NOT submit any DML statements (INSERT, UPDATE, DELETE, DROP, etc) to the database.
+If the question question is difficult for you to understant, or does not seem related to the database contents, simply return "I don't know" as the answer."""
 
 FORMAT_INSTRUCTIONS = """Use a Markdown JSON snippet to specify a tool by providing an "action" key (tool name) and an "action_input" key (tool input).
 
 Valid "action" values are: any of the {tool_names}.
 
-Provide only ONE action per $JSON_BLOB, formatted like:
+Provide only ONE action per JSON snippet, formatted like:
 
 ```json
 {{{{
@@ -21,16 +21,14 @@ Follow this format:
 
 User: input question to answer
 Thought: consider the origin question, previous steps and subsequent steps
-Action:
-```
-$JSON_BLOB
-```
+Action: provide a Markdown JSON snippet
 Observation: action result
 ... (repeat Thought/Action/Observation N times)
-Thought: I know the final answer
+Thought: Reach a conclusive understanding of the final answer
 Final Answer: Final response to user"""
 
-SUFFIX = """Take a deep breath and work on this problem step-by-step. You should first examine the database tables to identify which ones are most relevant for answering the question. Then, query the schema of the most promising tables to further understand their structure and contents. With this knowledge, construct appropriate {dialect} SQL queries to retrieve the information needed to answer the question.
+SUFFIX = """Take a deep breath and work on this problem step-by-step. You should first examine the database tables to identify which ones are most relevant for answering the question. Then, query the schemas of the most promising tables to further understand their structure and contents. With this knowledge, construct appropriate {dialect} SQL queries to retrieve the information needed to answer the question.
+Note that the information you need be distributed across multiple tables. In such cases, you will need to perform table joins to consolidate the information.
 Execute the queries, analyze the results, and derive the final answer. Let's begin!"""
 
 HUMAN_PREFIX = "User"
